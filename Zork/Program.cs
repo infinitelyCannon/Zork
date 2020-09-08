52cf95da597de0d7837880c705d46d6658a82e4f
Dakarai Simmons <infinitelyCannon@gmail.com>
2020-09-08 06:47:01 -0400

    Updated program to test out new command parser

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data.SqlTypes;
 
 namespace Zork
 {
@@ -21,20 +20,8 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            inputString = inputString.ToUpper();
-
-            if(inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if(inputString == "LOOK")
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
+            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Console.WriteLine(command);
         }
 
         private static Commands ToCommand(string commandString)
