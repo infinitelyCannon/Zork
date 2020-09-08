c06e6df66d9bfb8e10223a60dc7fdddd0c4c91b6
Dakarai Simmons <infinitelyCannon@gmail.com>
2020-09-08 07:01:22 -0400

    Implemented a game loop
    With parsed commands and everything.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -19,9 +19,35 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while(command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\n\n" +
+                            "A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = "You moved " + command;
+                        break;
+                    case Commands.UNKNOWN:
+                    default:
+                        outputString = "Unknown command";
+                        break;
+                }
+                Console.WriteLine(outputString);
+            }
         }
 
         private static Commands ToCommand(string commandString)
