7e8dffa0ec2c969253f2eba7cbe8526c4efbe63b
Dakarai Simmons <infinitelyCannon@gmail.com>
2020-09-10 02:58:02 -0400

    Promoted rooms into objects to add description properties

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -16,7 +16,7 @@ namespace Zork
 
     class Program
     {
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -32,16 +32,17 @@ namespace Zork
             Commands.WEST
         };
 
-        private static readonly string[,] Rooms = {
-            {"Rocky Trail", "South of House", "Canyon View"},
-            {"Forest", "West of House", "Behind House"},
-            {"Dense Woods", "North of House", "Clearing"}
-        };
+        private static readonly Room[,] Rooms = { 
+            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") }, 
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House") }, 
+            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") } };
+
         private static (int Row, int Column) Location = (1,1);
         
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            InitializeRoomDescriptions();
 
             Commands command = Commands.UNKNOWN;
             while(command != Commands.QUIT)
@@ -56,8 +57,7 @@ namespace Zork
                         Console.WriteLine("Thank you for playing!");
                         break;
                     case Commands.LOOK:
-                        Console.WriteLine("This is an open field west of a white house, with a boarded front door.\n\n" +
-                            "A rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
                     case Commands.NORTH:
                     case Commands.SOUTH:
@@ -105,5 +105,20 @@ namespace Zork
 
         private static Commands ToCommand(string commandString) =>
             Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail."; // Rocky Trail
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred."; // South of House
+            Rooms[0, 2].Description = "You are on at the top of the Great Canyon on its south wall."; // Canyon View
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you."; // Forest
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door."; // West of House
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar."; // Behind House
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight."; // Dense Woods
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred."; // North of House
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south."; // Clearing
+        }
     }
 }
---
+++ Zork/Room.cs
@@ -0,0 +1,16 @@
+﻿namespace Zork
+{
+    class Room
+    {
+        public string Name { get; }
+        public string Description { get; set; }
+
+        public override string ToString() => Name;
+
+        public Room(string name, string description = "")
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
