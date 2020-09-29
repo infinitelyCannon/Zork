43aa45dcdd8171a0f0ad2c26430103143e0d7fe3
Dakarai Simmons <infinitelyCannon@gmail.com>
2020-09-29 05:54:39 -0400

    Added new Json library to parse Room data in .json format.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace Zork
 {
@@ -44,28 +45,17 @@ namespace Zork
             RoomsFilename = 0
         }
 
-        private static readonly Room[,] Rooms = { 
+        private static Room[,] Rooms = { 
             { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") }, 
             { new Room("Forest"), new Room("West of House"), new Room("Behind House") }, 
             { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") } };
 
         private static (int Row, int Column) Location = (1,1);
-
-        private static readonly Dictionary<string, Room> RoomMap;
-
-        static Program()
-        {
-            RoomMap = new Dictionary<string, Room>();
-            foreach(Room room in Rooms)
-            {
-                RoomMap[room.Name] = room;
-            }
-        }
         
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-            const string defaultRoomsFilename = "Rooms.txt";
+            const string defaultRoomsFilename = "Rooms.json";
             string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
 
             InitializeRoomDescriptions(roomsFilename);
@@ -139,24 +129,7 @@ namespace Zork
         private static Commands ToCommand(string commandString) =>
             Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
 
-        private static void InitializeRoomDescriptions(string roomsFilename)
-        {
-            const string fileDelimiter = "##";
-            const int expectedFieldCount = 2;
-
-            string[] lines = File.ReadAllLines(roomsFilename);
-            foreach(string line in lines)
-            {
-                string[] fields = line.Split(fileDelimiter);
-
-                if (fields.Length != expectedFieldCount)
-                    throw new InvalidDataException("Invald record.");
-
-                string name = fields[(int)Fields.Name];
-                string description = fields[(int)Fields.Description];
-
-                RoomMap[name].Description = description;
-            }
-        }
+        private static void InitializeRoomDescriptions(string roomsFilename) =>
+            Rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
     }
 }
